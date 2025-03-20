940e5aa49d18d04a0be9de61b7ec411dac67e9d8
MartinKrustev06 <_email@example.com>
2025-03-20 19:14:00 +0200

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	src/main/resources/templates/login.html

--- src/main/java/com/koli4ka/app/web/UserController.java
+++ src/main/java/com/koli4ka/app/web/UserController.java
@@ -1,5 +1,7 @@
 package com.koli4ka.app.web;
 
+import com.koli4ka.app.user.model.User;
+import com.koli4ka.app.user.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -9,12 +11,18 @@ import java.util.UUID;
 
 @Controller
 public class UserController {
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
 
     @GetMapping("/users/{id}/")
     public ModelAndView getProfile(@PathVariable UUID id) {
         ModelAndView mav = new ModelAndView();
-        mav.addObject("id", id);
-//        mav.setViewName("");
+        User user=userService.getById(id);
+        mav.addObject("user",user);
+        mav.setViewName("user-profile");
         return mav;
 
 
